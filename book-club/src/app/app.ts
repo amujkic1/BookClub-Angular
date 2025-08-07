42f2f8f5ba80309b640fdbe27da99e65552ce3bc
amujkic1 <amujkic1@etf.unsa.ba>
2025-08-07 17:26:10 +0200

    first commit

---
+++ book-club/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Home],
+  
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('book_club');
+}
